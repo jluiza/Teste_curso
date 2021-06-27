bcdb65c66f83f74abee24e53ae94ee7034971c2e
jluiza <47866563+jluiza@users.noreply.github.com>
2021-06-27 18:52:26 -0300

    Inicio programa dados básicos

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Int - 156 como 32334
+            // Float - -15.6 como 232.132 ==> Double\ Decimal
+            //Bool - true or false
+            //String - "textual"
+            //Char - caracter 'a'
+
+            var meuTexto = "Hello World";
+
+            Console.WriteLine(meuTexto);
+            Console.ReadKey();
+        }
+    }
+}
