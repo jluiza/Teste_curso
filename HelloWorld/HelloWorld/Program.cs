ca94cd0c00a157340d81daee3bd3d73ef617dc00
jluiza <47866563+jluiza@users.noreply.github.com>
2021-09-12 00:19:58 -0300

    Novas instruções sobre variáveis

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -16,10 +16,53 @@ namespace HelloWorld
             //String - "textual"
             //Char - caracter 'a'
 
+            //A-Z, 0 -9 desde que não seja no inicio, _, não @#$%
+            /*
+            int segundaGuerraMundial = 1945;
+            string corFavorita = "Preto";
+            float velocidade = 245.2f;
+            bool sGM = true;
+
             var meuTexto = "Hello World";
 
-            Console.WriteLine(meuTexto);
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(corFavorita);
+            Console.WriteLine(velocidade);
+            Console.WriteLine(sGM);
+
             Console.ReadKey();
+
+            corFavorita = "Roxo";
+            velocidade = 90.2f;
+
+            Console.WriteLine(corFavorita);
+            Console.ReadKey();
+            */
+
+            /*
+            var corFavorita = "Vermelho";
+            var modeloDoProduto = 2323;
+            */
+
+            //não aconselhavel usar dynamic
+            /*
+            dynamic corFav = "Vermelho";
+            Console.WriteLine(corFav);
+            corFav = 22;
+            Console.WriteLine(corFav);
+
+            const float PI = 3.145254;
+            */
+
+            string nome = "";
+
+            Console.WriteLine("Escreva seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Seu nome é: ");
+            Console.WriteLine(nome);
+
+            Console.ReadKey();
+        
         }
     }
 }
